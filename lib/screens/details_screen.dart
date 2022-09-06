411e75a943ad4e936328c6009a64ac13be0e56c7
Ashot Khachatryan <ashot199514@gmail.com>
2022-09-06 11:53:38 +0400

    Update User Details and use TextFieldForm

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -16,6 +16,10 @@ class DetailsScreen extends StatefulWidget {
 
 class _DetailsScreenState extends State<DetailsScreen> {
   late DetailsBloc _detailsBloc;
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+  final TextEditingController _locationController = TextEditingController();
 
   @override
   void initState() {
@@ -67,17 +71,69 @@ class _DetailsScreenState extends State<DetailsScreen> {
   }
 
   Widget _renderDeatils({required User user}) {
+    _nameController.text = user.name;
+    _emailController.text = user.email;
+    _phoneController.text = user.phone;
+    _locationController.text = user.address.city + user.address.street;
+
     return Padding(
         padding: const EdgeInsets.all(20),
         child: Column(children: [
-          Row(children: [const Text('User name: '), Text(user.name)]),
-          Row(children: [const Text('User email: '), Text(user.email)]),
-          Row(children: [const Text('User email: '), Text(user.phone)]),
           Row(children: [
-            const Text('User email: '),
-            Text('${user.address.city}, ${user.address.street}')
+            const Text('Name: '),
+            const SizedBox(
+              width: 20,
+            ),
+            _renderName()
+          ]),
+          Row(children: [
+            const Text('Email: '),
+            const SizedBox(width: 20),
+            _renderEmail()
+          ]),
+          Row(children: [
+            const Text('Phone: '),
+            const SizedBox(width: 20),
+            _renderPhone()
+          ]),
+          Row(children: [
+            const Text('Location: '),
+            const SizedBox(width: 20),
+            _renderLocation()
           ]),
         ]));
+  }
+
+  Widget _renderName() {
+    return Expanded(
+        child: TextFormField(
+            controller: _nameController,
+            readOnly: true,
+            decoration: const InputDecoration(border: InputBorder.none)));
+  }
+
+  Widget _renderEmail() {
+    return Expanded(
+        child: TextFormField(
+            controller: _emailController,
+            readOnly: true,
+            decoration: const InputDecoration(border: InputBorder.none)));
+  }
+
+  Widget _renderPhone() {
+    return Expanded(
+        child: TextFormField(
+            controller: _phoneController,
+            readOnly: true,
+            decoration: const InputDecoration(border: InputBorder.none)));
+  }
+
+  Widget _renderLocation() {
+    return Expanded(
+        child: TextFormField(
+            controller: _locationController,
+            readOnly: true,
+            decoration: const InputDecoration(border: InputBorder.none)));
   }
 
   void _onEditTap() {}
